856b27a806c8621863bb4ae8b187c6ff596db273
Betzywj <136661742+Betzywj@users.noreply.github.com>
2023-10-20 16:07:20 -0600

    20102023
    20102023

---
+++ app/php/footer.php
@@ -0,0 +1,19 @@
+
+<footer class="footer  ">
+    <div class="row">
+
+        
+        <div class="cont">
+            <a class="">
+                <img src="img/Logotipo Blanco.png" alt="Logo" width="150px" style="margin-left: 2rem;">
+            </a>
+            <a class="">
+                <p class="text-sm-left derechos">PROGRAMA PARA EL DESARROLLO DE LA SUSTENTABILIDAD EN LA CADENA DE SUMINISTRO </p>
+            </a>
+            <a class="m-3">
+                <small style="color: var(--procasu3); margin-top: 3rem; margin-left: 3rem;">&copy; 2023-2024 <b>Procasu</b> Todos los Derechos Reservados.</small>
+            </a>
+        </div>
+
+    </div>
+</footer>
