0361ab88fd9f8ff6b491876da41e7a380bf5b903
Betzywj <136661742+Betzywj@users.noreply.github.com>
2023-10-20 16:06:25 -0600

    20102023
    20102023

---
+++ app/php/conoce.php
@@ -0,0 +1,57 @@
+<?php include_once("nav.php") ?>
+
+<div class="">
+
+
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO PROPÓSITO.png" alt="" class="iconos" style="margin-right: .5rem;">
+                PROPÓSITO</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO PROCASU.png" alt="" class="iconos" style="margin-right: .5rem;">
+                PROCASU</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO NIVELES DE MEDICIÓN.png" alt="" class="iconos" style="margin-right: .5rem;">
+                NIVELES DE MEDICIÓN</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO ROLES.png" alt="" class="iconos" style="margin-right: .5rem;">
+                ROLES</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO ALCANCE.png" alt="" class="iconos" style="margin-right: .5rem;">
+                ALCANCE</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO MODELO DE GESTIÓN.png" alt="" class="iconos" style="margin-right: .5rem;">
+                NIVELES DE GESTIÓN</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active mx-5 my-4 br rounded" href="#" style="padding:2rem; background-color: var(--procasu1); text-align: center; color:var(--procasu3);">
+                <img src="Iconos/Iconos barra conoce procaso/ICONO BENEFICIOS.png" alt="" class="iconos" style="margin-right: .5rem;">
+                BENEFICIOS</a>
+        </li>
+    </ul>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+
+<?php include_once("footer.php"); ?>
