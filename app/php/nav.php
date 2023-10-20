e0cd788c3ef0fba1c1ccc29e1d8491bae1840476
betzy <karinaandres111@gmail.com>
2023-10-20 16:03:30 -0600

    20102023

---
+++ app/php/nav.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style-nav.css">
+    <title>PROCASU</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg  ">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo Procasu.png" alt="logo" width="150px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <div class="" style="margin-top: 1rem; margin-left: 3rem;">
+                    <p class="text-sm-left">PROGRAMA PARA EL DESARROLLO DE LA SUSTENTABILIDAD EN LA CADENA DE SUMINISTRO </p>
+                </div>
+        </div>
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0 content">
+            <li class="nav-item dropdown">
+                <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="Iconos/Icono-Usuario.png " class="img-fluid rounded-circle avatar mr-2" alt="imagen" />
+                    usuario
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="perfil1.php">Mi perfil</a>
+                    <a class="dropdown-item" href="#">Expediente</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Cerrar sesión</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="d-flex">
+
+        <div id="sidebar-container" class=" d-inline-block  d-flex ">
+            <div class="menu ">
+                <br>
+                <a href="#" class="m-3 d-inline-block btn  d-block  " style="padding:1rem; background-color: var(--procasu3); text-align: center; ">
+                    <img src="Iconos/Herramientas PROCASU.png" alt="herramientas" class="iconos " style="margin-right: .5rem;">
+                    Herramientas PROCASU
+                </a>
+                <br>
+                <a href="#" class="m-3 d-inline-block btn d-block   " style="padding:1rem; background-color: var(--procasu3); margin-left: rem;  ">
+                    <img src="Iconos/Biblioteca Digital.png" alt="herramientas" class="iconos " style="margin-right: 3rem;">
+                    Biblioteca Digital
+                </a>
+                <br>
+                <a href="conoce.php" class="m-3 d-inline-block btn d-block " style="padding:1rem; background-color: var(--procasu3);   ">
+                    <img src="Iconos/Conoce PROCASU.png" alt="herramientas" class="iconos " style="margin-right: 2rem;">
+                    Conoce PROCASU
+                </a>
+                <br>
+                <a href="#" class="m-3 d-inline-block btn  d-block " style="padding:1rem; background-color: var(--procasu3); ">
+                    <img src="Iconos/Necesitas Ayuda.png" alt="herramientas" class="iconos " style="margin-right: 2rem;">
+                    ¿Necesitas Ayuda?
+                </a>
+            </div>
+        </div>
+
+
+
+
+
+</body>
