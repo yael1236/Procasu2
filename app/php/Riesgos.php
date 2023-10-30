cdcab9f724e6f7cd3cf2c62045e53ec9e0a0506c
Mari031116 <lizet11603@gmail.com>
2023-10-30 09:08:39 -0600

    006

--- app/php/Riesgos.php
+++ app/php/Riesgos.php
@@ -1,88 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-
-  <head>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.3/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Styles -->
-  <link rel="stylesheet" href="../../css/stylein.css">
-  <link rel="stylesheet" href="../../style-nav.css">
-
-  <!-- Google fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
-
-  <!-- Ionic icons -->
-  <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-  <!-- Bootstrap Iconos -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-	<meta charset="UTF-8">
-  <title>Riesgos</title>
-  </head>
-    <body>
-    <!--<body onload="alert('Intrucciones')"> -->
-    <div class="d-flex" id="content-wrapper">
-    <!-- MENU -->
-    <div id="sidebar-container" class="bg-primary" style=" top:0; height:auto;">
-    <div class="logo">
-    <h4 class="text-light font-weight-bold mb-0">PROCASU</h4>
-    </div>
-    <div class="menu">
-    <a href="#" class="m-3 d-inline-block btn  d-block  " style="padding:1rem; background-color: var(--procasu3); text-align: center; ">
-    <img src="../../img/Iconos_png/Icono-Herramientas-PROCASU.png" alt="herramientas" class="iconos " style="margin-right: .5rem;">
-    Herramientas PROCASU
-    </a>
-    <br>
-    <br>
-    <a href="#" class="m-3 d-inline-block btn d-block   " style="padding:1rem; background-color: var(--procasu3); margin-left: rem;  ">
-    <img src="../../img/Iconos_png/Icono-Biblioteca-Digital.png" alt="herramientas" class="iconos " style="margin-right: 3rem;">
-    Biblioteca Digital
-    </a>
-    <br>
-    <a href="#" class="m-3 d-inline-block btn d-block " style="padding:1rem; background-color: var(--procasu3);   ">
-    <img src="../../img/Iconos_png/Icono-Conoce-PROCASU.png" alt="herramientas" class="iconos " style="margin-right: 2rem;">
-    Conoce PROCASU
-    </a>
-    <br>
-    <a href="../php/contacto.php" class="m-3 d-inline-block btn  d-block " style="padding:1rem; background-color: var(--procasu3); ">
-    <img src="../../img/Iconos_png/Icono-Necesitas-Ayuda.png" alt="herramientas" class="iconos " style="margin-right: 2rem;">
-    ¿Necesitas Ayuda?
-    </a>
-    </div>
-    </div>
-    <!-- Fin MENU -->
-    <div class="w-100">
-
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg">
-    <a class="navbar-brand" href="#">
-    <img src="../../img/logo Procasu.png" alt="logo" width="150px">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    <div class="" style="margin-top: 1rem; margin-left: 3rem;">
-    <p class="text-sm-left">PROGRAMA PARA EL DESARROLLO DE LA SUSTENTABLIDAD EN LA CADENA DE SUMINISTROS</p>
-    </div>
-    </div>
-    <ul class="navbar-nav ml-auto mt-2 mt-lg-0 content">
-    <li class="nav-item dropdown">
-    <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <img src="../../img/Icono-Usuario.png" class="img-fluid rounded-circle avatar mr-2" alt="imagen" />
-    usuario
-    </a>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-    <a class="dropdown-item" href="#">Mi perfil</a>
-    <a class="dropdown-item" href="#">Expediente</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">Cerrar sesión</a>
-    </div>
-   </li>
-    </ul>
-    </nav>
+<?php include_once("../../app/php/nav.php") ?> 
     <!-- fin de la barra de navegacion--> 
                               <!-- VARIABLES -->   
                               <?php 
@@ -127,7 +43,7 @@
                               <th class="text-center" scope="col">Control 1</th>
                               <th class="text-center" scope="col">Control 2</th>
                               <th class="text-center" scope="col">Control 3 </th>
-                              <th class="text-center" scope="col"><div class="btn-group">Probabilidad</th>
+                              <th class="text-center" scope="col">Probabilidad</th>
                               <th class="text-center" scope="col">Impacto</th>
                               <th class="text-center" scope="col">Evaluacion</th>
                               <th class="text-center" scope="col">Responsable</th>
@@ -320,57 +236,8 @@
                               <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                               </svg>Regresar </a>               
                               </main>
-                              <!-- PIE -->
-                              <footer >
-                              <br>
-					                    <p class="text-center" style="width: 100%;">Copyright <sup>©</sup> 2023-2024 Procasu todos los derechos reservados </p>
-                              </footer>
                               </div> 
-    <!-- FUN NAV --> 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
-    <script>
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, { 
-    type: 'bar',
-    data: {
-    labels: ['Feb 2020', 'Mar 2020', 'Abr 2020', 'May 2020'],
-    datasets: [{
-    label: 'Nuevos usuarios',
-    data: [50, 100, 150, 200],
-    backgroundColor: [
-    '#12C9E5',  
-    '#12C9E5',
-    '#12C9E5',
-    '#111B54'
-    ],
-   maxBarThickness: 30,
-    maxBarLength: 2
-    }]
-    },
-   options: {
-   scales: {
-    yAxes: [{
-    ticks: {
-    beginAtZero: true
-    }
-    }]
-    }
-    }
-    });
-    </script>
-    </div>
-    </div>     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</div>
   </body> 
+  <?php include_once("../../app/php/footer.php") ?> 
 </html>
--- app/php/contacto.php
+++ app/php/contacto.php
@@ -70,12 +70,7 @@ if (isset($_POST['submit'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="es">
-
-
-
-<div class="d-flex" id="content-wrapper"> 
+<?php include_once("../../app/php/nav.php") ?> 
 
           <main style="height:auto; width:auto;">
           <div style="display: flex; margin:none; padding:none; height:90%; ">
@@ -137,7 +132,7 @@ if (isset($_POST['submit'])) {
                         </div>
                     </form>         
 								</div>
-                                <section style="width: 18rem; height:10rem; color:black; font-size:1.6rem;">
+                                <section style="width: 20rem; height:10rem; color:black; font-size:1.6rem;">
 									<div class="container aline:left;">
 										<div class="row">
 											<div class="col-2 text-right">
@@ -185,56 +180,10 @@ if (isset($_POST['submit'])) {
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-  <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
-        <script>
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var myChart = new Chart(ctx, { 
-                type: 'bar',
-                data: {
-                    labels: ['Feb 2020', 'Mar 2020', 'Abr 2020', 'May 2020'],
-                    datasets: [{
-                        label: 'Nuevos usuarios',
-                        data: [50, 100, 150, 200],
-                        backgroundColor: [
-                            '#12C9E5',  
-                            '#12C9E5',
-                            '#12C9E5',
-                            '#111B54'
-                        ],
-                        maxBarThickness: 30,
-                        maxBarLength: 2
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
-            </script>
+    
+    
 </body>
-<footer >
-            <br>
-					<p class="text-center">Copyright <sup>©</sup> 2023-2024 Procasu todos los derechos reservados </p>
-				
-            </footer>
+<?php include_once("../../app/php/footer.php") ?>   
 
 </html>
     
